c816ea28c53634cd866e70a432a709178d951b7d
Bojan Bilić <bojan@projectzagreb.com>
2016-06-03 21:16:47 +0200

    Initial Commit

---
+++ ma3x/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ma3x
+//
+//  Created by Bojan Bilić on 03/06/16.
+//  Copyright © 2016 Bojan Bilić. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
